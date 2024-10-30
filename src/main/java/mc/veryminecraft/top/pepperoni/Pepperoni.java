772028f3d9e58da786ced9faa5efbaefa08edc89
GreshAnt <ccvvxuruoteng@gmail.com>
2024-10-30 05:46:37 +0000

    update

---
+++ src/main/java/mc/veryminecraft/top/pepperoni/Pepperoni.java
@@ -0,0 +1,10 @@
+package mc.veryminecraft.top.pepperoni;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Pepperoni implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/mc/veryminecraft/top/pepperoni/client/PepperoniClient.java
@@ -0,0 +1,10 @@
+package mc.veryminecraft.top.pepperoni.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PepperoniClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
